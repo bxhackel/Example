ce320a900bd61f275f3f612d0978ab13a0461115
bxhackel <103291716+bxhackel@users.noreply.github.com>
2022-04-28 02:25:13 -0700

    init commit

---
+++ Example.java
@@ -0,0 +1,5 @@
+public class Example {
+    public boolean test() {
+        return false;
+    }
+}
---
+++ ExampleTester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ExampleTester {
+    @Test
+    public void testTest() {
+        Example e = new Example();
+        assertTrue(e.test());
+    }
+}
